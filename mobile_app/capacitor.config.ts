23014b541d2a475b5b3d98665c64bc4da38fc007
DESKTOP-15TBAPO\user <saiyandra303@gmail.com>
2025-11-26 10:03:46 -0800

    remove wrong code

--- mobile_app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.react.app',
-  appName: 'react',
-  webDir: 'out'
-};
-
-export default config;
--- mobile_app/src/router.tsx
+++
@@ -1,7 +0,0 @@
-import { useRoutes } from 'react-router-dom';
-import routes from './router/config';
-
-export const AppRoutes = () => {
-  const element = useRoutes(routes);
-  return element;
-};
